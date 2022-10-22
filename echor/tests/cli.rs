acfa343b0bb9be9ba9217c2efdf83e9c0832eca4
raulminan <raulminan@gmail.com>
2022-10-22 22:29:45 +0200

    feat: add echo-rust

---
+++ echor/tests/cli.rs
@@ -0,0 +1,55 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+#[test]
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
+    // runs the programs and asserts that it fails and it prints a usage statemen to STDERR
+    cmd.assert() 
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
+        Ok(()) // omit semicolon to directly return a value
+}
+
+#[test]
+fn runs() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
+    // run echor and verify it exits successfully
+    cmd.arg("Hello").assert().success();
+    Ok(())
+}
+
+
+// create helper function 
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
+
+#[test]
+fn hello1() -> TestResult {
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
+}
