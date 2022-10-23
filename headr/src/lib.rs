b7fc18b1ba591f5d5f31db7599898ec1761fff5e
raulminan <raulminan@gmail.com>
2022-10-23 20:46:19 +0200

    chore: start working in headr

---
+++ headr/src/lib.rs
@@ -0,0 +1,105 @@
+use clap::{App, Arg};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>, // optional usize
+}
+
+// -----------------------------------------------------------------------------
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("headr")
+        .version("0.1.0")
+        .author("raulminan")
+        .about("head written in Rust")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .default_value("10"),
+                //.conflicts_with("bytes"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .help("Number of bytes")
+                .conflicts_with("lines")
+                .takes_value(true) 
+                // .takes_value is needed here and not in the others
+                // bc it's of type Option<()>
+                
+        )
+        .get_matches();
+
+    let lines = matches
+        .value_of("lines")
+        .map(parse_positive_int) // convert str to int
+        .transpose() 
+        .map_err(|e| format!("illegal line count -- {}", e))?;
+    
+    let bytes = matches
+        .value_of("bytes")
+        .map(parse_positive_int)
+        .transpose() // whyyyy
+        .map_err(|e| format!("illegal byte count -- {}", e))?;
+
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        lines: lines.unwrap(),
+        bytes,
+    })
+}
+
+// -----------------------------------------------------------------------------
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
+
+// --------------------------------------------------
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse() {
+        Ok(n) if n > 0 => Ok(n),
+        _ => Err(From::from(val)),
+        //   other options:
+        //   Err(val.into())
+        //   Err(Into::into(val))
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    // 3 is an OK integer
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    // any string is an error
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    // 0 is an error
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
+}
