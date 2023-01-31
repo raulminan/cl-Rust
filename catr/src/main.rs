a4258368c3f160bb13e1c56fa5dc4aac597094ae
raulminan <raulminan@gmail.com>
2023-01-31 19:38:05 +0100

    restart repo after reading the book

--- catr/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // check if return value of catr::run matches Err(e)
-    // where e is some value that implemets the Error trait
-    if let Err(e) = catr::get_args().and_then(catr::run) {
-        eprintln!("{}", e); // error print line
-        std::process::exit(1);
-    }
-}
