adfd5cd1e5a09000ea5c0ec7e931c5fcc8ab9177
raulminan <raulminan@gmail.com>
2023-02-02 15:35:00 +0100

    finish wcr

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -34,19 +34,77 @@ pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
     let mut num_words = 0;
     let mut num_bytes = 0;
     let mut num_chars = 0;
+    let mut line = String::new();
+
+    loop {
+        let line_bytes = file.read_line(&mut line)?;
+        if line_bytes == 0 {
+            break;
+        }
+        
+        num_bytes += line_bytes;
+        num_lines += 1;
+        num_words += line.split_whitespace().count();
+        num_chars += line.chars().count();
+        line.clear();
+    }
 
     Ok(FileInfo { num_lines, num_words, num_bytes, num_chars })
 }
 
+fn format_field(value: usize, show: bool) -> String {
+    if show {
+        format!("{:>8}", value)
+    } else {
+        "".to_string()
+    }
+}
 
 
 pub fn run(config: Config) -> MyResult<()> {
-    for filename in config.files {
-        match open(&filename) {
-            Err(e) => eprint!("Failed to open {}: {}", filename, e),
-            Ok(_) => println!("Opened {}", filename),
+    let mut total_lines = 0;
+    let mut total_words = 0;
+    let mut total_bytes = 0;
+    let mut total_chars = 0;
+
+    for filename in &config.files {
+        match open(filename) {
+            Err(e) => eprintln!("Failed to open {}: {}", filename, e),
+            Ok(file) => {
+                if let Ok(info) = count(file) {
+                    println!(
+                        "{}{}{}{}{}",
+                        format_field(info.num_lines, config.lines),
+                        format_field(info.num_words, config.words),
+                        format_field(info.num_bytes, config.bytes),
+                        format_field(info.num_chars, config.chars),
+                        if filename == "-" {
+                            "".to_string()
+                        } else {
+                            format!(" {}", &filename)
+                        }
+                    );
+
+                    total_lines += info.num_lines;
+                    total_words += info.num_words;
+                    total_bytes += info.num_bytes;
+                    total_chars += info.num_chars;
+
+                }
+            }
         }
     }
+
+    if config.files.len() > 1 {
+        println!(
+            "{}{}{}{} total",
+            format_field(total_lines, config.lines),
+            format_field(total_words, config.words),
+            format_field(total_bytes, config.bytes),
+            format_field(total_chars, config.chars)
+        );
+    }
+
     Ok(())
 }
 
@@ -72,7 +130,6 @@ pub fn get_args() -> MyResult<Config> {
         )
         .arg(
             Arg::with_name("words")
-                .value_name("WORDS")
                 .help("Show number of words")
                 .takes_value(false)
                 .short("w")
@@ -80,7 +137,6 @@ pub fn get_args() -> MyResult<Config> {
         )
         .arg(
             Arg::with_name("bytes")
-                .value_name("BYTES")
                 .help("Show numebr of bytes")
                 .takes_value(false)
                 .short("c")
@@ -88,7 +144,6 @@ pub fn get_args() -> MyResult<Config> {
         )
         .arg(
             Arg::with_name("chars")
-                .value_name("CHARS")
                 .help("Show number of characters")
                 .takes_value(false)
                 .short("m")
@@ -102,7 +157,7 @@ pub fn get_args() -> MyResult<Config> {
     let mut bytes = matches.is_present("bytes"); 
     let chars = matches.is_present("chars");
 
-    if [lines, words, bytes, chars].iter().all(|v| v == &false) {
+    if [words, bytes, chars, lines].iter().all(|v| v == &false) {
         // equivalente a lambda x: x == False.
         // the values are references, so they have to be compared to &false
         lines = true;
@@ -124,7 +179,7 @@ pub fn get_args() -> MyResult<Config> {
 
 #[cfg(test)]
 mod tests {
-    use super::{count, FileInfo};
+    use super::{count, format_field, FileInfo};
     use std::io::Cursor;
 
     #[test]
@@ -140,5 +195,12 @@ mod tests {
         };
 
         assert_eq!(info.unwrap(), expected)
+    }
+
+    #[test]
+    fn test_format_field() {
+        assert_eq!(format_field(1, false), "");
+        assert_eq!(format_field(3, true), "       3");
+        assert_eq!(format_field(10, true), "      10");
     }
 }
